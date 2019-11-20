247147a2c8aa3c0aaed2dc07c619abf30ab95dba
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-20 23:21:54 +0400

    Add some styles

--- Shop/Controllers/CarsController.cs
+++ Shop/Controllers/CarsController.cs
@@ -25,7 +25,7 @@ namespace Shop.Controllers
 
             CarsListViewModel obj = new CarsListViewModel();
             obj.AllCars = _allCars.Cars;
-            obj.CurrCategory = "Car";
+            obj.CurrCategory = "Cars";
             return View(obj);
         }
     }
