d3fa4d778e69abceb1826f42cf34f67f95a02840
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-20 18:09:18 +0400

    Creating Controller and View for start

---
+++ Shop/Controllers/CarsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategory;
+
+        public CarsController(IAllCars allCars, ICarsCategory allCategory)
+        {
+            this._allCars = allCars;
+            this._allCategory = allCategory;
+        }
+
+        public ViewResult List()
+        {
+            var cars = _allCars.Cars;
+            return View(cars);
+        }
+    }
+}
