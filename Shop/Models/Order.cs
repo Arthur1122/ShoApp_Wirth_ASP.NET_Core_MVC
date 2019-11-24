25e066149b85deffce266bc3f10720f989ef76a3
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-24 23:43:36 +0400

    Validation for order

--- Shop/Models/Order.cs
+++ Shop/Models/Order.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +9,40 @@ namespace Shop.Models
 {
     public class Order
     {
+        [BindNever]
         public int Id { get; set; }
+
+        [Display(Name = "Enter Name")]
+        [StringLength(7)]
+        [Required(ErrorMessage = "Name length at least 7 characters")]
         public string Name { get; set; }
+
+        [Display(Name = "Enter SurName")]
+        [StringLength(10)]
+        [Required(ErrorMessage = "Name length at least 10 characters")]
         public string SurName { get; set; }
+
+        [Display(Name = "Enter Name")]
+        [StringLength(7)]
+        [Required(ErrorMessage = "Name length at least 7 characters")]
         public string  Address { get; set; }
+
+        [Display(Name = "Enter Phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(10)]
+        [Required(ErrorMessage = "Phone number at least 10 characters")]
         public string Phone { get; set; }
+
+        [Display(Name = "Enter Email")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(15)]
+        [Required(ErrorMessage = "Name length at least 15 characters")]
         public string  Email { get; set; }
+
+        [BindNever]
+        [ScaffoldColumn(false)]
         public DateTime OrderTime { get; set; }
+        
         public List<OrderDetail> OrderDetails { get; set; }
     }
 }
