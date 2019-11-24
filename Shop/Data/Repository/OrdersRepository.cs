5c57071f771205767a9e83731c2739ae4db6be8b
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-11-24 20:53:12 +0400

    Implemented CreateOrder method

---
+++ Shop/Data/Repository/OrdersRepository.cs
@@ -0,0 +1,42 @@
+﻿using Shop.Interfaces;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Repository
+{
+    public class OrdersRepository : IAllOrders
+    {
+        private readonly AppDbContext _context;
+        private readonly ShopCarts _shopCarts;
+        public OrdersRepository(AppDbContext context, ShopCarts shopCarts)
+        {
+            this._context = context;
+            this._shopCarts = shopCarts;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderTime = DateTime.Now;
+            _context.Orders.Add(order);
+
+            var items = _shopCarts.ListShopItems;
+
+            foreach (var el in items)
+            {
+                var orderDet = new OrderDetail()
+                {
+                    CarId = el.Car.Id,
+                    Price = el.Car.Price,
+                    OrderId = order.Id
+                };
+
+                _context.OrderDetails.Add(orderDet);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
